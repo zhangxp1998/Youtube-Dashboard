3b8960f27bf2f7bafdbfa9e877f64f1337a40b55
Kelvin Zhang <zhangxp1998@gmail.com>
2017-11-03 17:37:52 -0500

    Initial setup

---
+++ reactjs/containers/SampleAppContainer.jsx
@@ -0,0 +1,45 @@
+import React from "react"
+import Radium from "radium"
+
+import { connect } from "react-redux"
+
+import * as counterActions from "../actions/counterActions"
+import Headline from "../components/Headline"
+
+const styles = {
+  button: {
+    cursor: "pointer",
+  },
+  counter: {
+    color: "blue",
+    fontSize: "20px",
+  }
+}
+
+@connect(state => ({
+  counters: state.counters,
+}))
+@Radium
+export default class SampleAppContainer extends React.Component {
+  handleClick() {
+    let {dispatch} = this.props;
+    dispatch(counterActions.increaseCounter())
+  }
+
+  render() {
+    let {counters} = this.props
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            <Headline>Sample App!</Headline>
+            <div style={[styles.button]} onClick={() => this.handleClick()}>INCREASE</div>
+            <code style={[styles.counter]}>{counters.clicks}</code>
+            <p>{process.env.BASE_API_URL}</p>
+            <p>Hello World~</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
