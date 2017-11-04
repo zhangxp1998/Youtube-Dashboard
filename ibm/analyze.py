984b441a3699279aa257197beb15b85dbf5b8737
William He <will@1x-nat-vl932-172-30-250-23.wireless.umass.edu>
2017-11-04 04:49:50 -0400

    added tone translation code

---
+++ ibm/analyze.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import json
+import os
+import sys
+from os.path import join, dirname
+from watson_developer_cloud import ToneAnalyzerV3
+from matplotlib import pyplot as plt
+from googletrans import Translator
+
+
+# Variables
+tone_analyzer = ToneAnalyzerV3(
+    username='35187b3a-9705-45d1-8e10-5d61c3ae91d3',
+    password='DiPjetgprO3i',
+    version='2017-09-21')
+
+
+def main():
+    print("Analyzing Text from " + sys.argv[1])
+    file_name = sys.argv[1]
+    data = []
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+        size = len(lines)
+        data = []
+        tr = Translator()
+        for text in lines:
+            text = tr.translate(text).text
+            data.append(tone_analyzer.tone(text=text))
+
+    emotives = []
+    emotive_stats = {}
+    for j in data:
+        for t in j['document_tone']['tones']:
+            emotives.append(t)
+            if t['tone_name'] not in emotive_stats:
+                emotive_stats[t['tone_name']] = t['score']
+            else:
+                emotive_stats[t['tone_name']] += t['score']
+            # for tone in t:
+            #     if tone['tone_name'] not in emotive_types:
+            #         emotive_types.append(tone['tone_name'])
+    print emotive_stats
+        # print json.dumps(j, indent=4, sort_keys=True)
+    plt.bar(range(len(emotive_stats)), emotive_stats.values(), align='center')
+    plt.xticks(range(len(emotive_stats)), emotive_stats.keys())
+
+    plt.show()
+    return
+
+
+if __name__ == "__main__":
+    main()
